533a8314f28032a8063153bb1a6b2f1f9bbd80c1
claudiuintech <moisaclaudiu2309@gmail.com>
2020-11-29 22:52:05 +0200

    frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import Services from './Services'
+
+function App() {
+  return (
+    <div id="app-wrapper">
+     <h1>Money maker</h1>
+     <Router>
+       
+     </Router>
+    </div>
+  );
+}
+
+export default App;
