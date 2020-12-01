1deff8fcdc6acd5ebd7526e1f6d053befc72b32c
Daniel Moisa <danimoisa93@gmail.com>
2020-12-01 21:32:33 +0200

    add helmet

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,13 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   // Enable cors on development only
   app.enableCors();
