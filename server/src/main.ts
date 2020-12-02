9af65775e4fd66fc678c67b9e37ce183aa5b58ee
Daniel Moisa <danimoisa93@gmail.com>
2020-12-02 21:24:31 +0200

    added rate limmit

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
 import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
 
+  // Enable security packages
   app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 10, // limit each IP to 100 requests per windowMs
+    }),
+  );
 
   // Enable cors on development only
   app.enableCors();
